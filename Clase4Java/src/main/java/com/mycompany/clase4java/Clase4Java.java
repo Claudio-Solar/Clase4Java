1a5a83668f56c8312ae25c1b7b330c3125e51b07
Claudio-Solar <cv.solar16@gmail.com>
2022-06-26 22:08:30 -0500

    clase4
    clase4

--- Clase4Java/src/main/java/com/mycompany/clase4java/Clase4Java.java
+++ Clase4Java/src/main/java/com/mycompany/clase4java/Clase4Java.java
@@ -12,6 +12,7 @@ package com.mycompany.clase4java;
 public class Clase4Java {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+      Formularios.FormulariosGeneral objetoFormularioGeneral= new Formularios.FormulariosGeneral();
+      objetoFormularioGeneral.setVisible(true);
     }
 }
